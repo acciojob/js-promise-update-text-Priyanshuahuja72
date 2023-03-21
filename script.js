c87b230d25b6cacbce0f7bed123acce90c2fbab0
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-03-21 20:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+let output = document.getElementById("output");
+function promise1()
+{
+    return new Promise((resolve , reject) => {
+        setTimeout(() => {
+           resolve(
+            output.innerHTML = "Hello, world!"
+           )
+        }, 1000)
+    })
+}
+promise1();
+}
